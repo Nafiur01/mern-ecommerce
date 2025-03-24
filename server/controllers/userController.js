c803eedd9fa95fc22275e1bfc6f015a9e5454620
Nafiur01 <nafiurrahman373@gmail.com>
2025-03-24 22:14:05 +0600

    Refactoring and reusability, making dynamic

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -51,11 +51,11 @@ const getUsers = async (req, res, next) => {
   }
 };
 
-const getUser = async (req, res, next) => {
+const getUserById = async (req, res, next) => {
   try {
     const id = req.params.id;
     const options = { password: 0 };
-    const user = await findWithId(id, options);
+    const user = await findWithId(User,id, options);
 
     return successResponse(res, {
       statusCode: 200,
@@ -67,11 +67,11 @@ const getUser = async (req, res, next) => {
   }
 };
 
-const deleteUser = async (req, res, next) => {
+const deleteUserById = async (req, res, next) => {
   try {
     const id = req.params.id;
     const options = { password: 0 };
-    const user = await findWithId(id, options);
+    const user = await findWithId(User,id, options);
 
     const userImagePath = user.image;
     fs.access(userImagePath, (err) => {
@@ -97,4 +97,4 @@ const deleteUser = async (req, res, next) => {
     next(error);
   }
 };
-module.exports = { getUsers, getUser, deleteUser };
+module.exports = { getUsers, getUserById, deleteUserById };
